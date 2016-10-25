4e6c5f3c6d68b74d9f8ffcaeaaf9f163b2cf28c1
Guilherme Berger <guilherme.berger@gmail.com>
2016-10-25 19:25:04 -0200

    change example

--- examples/example.lua
+++ examples/example.lua
@@ -3,25 +3,11 @@ function innermacro(v)
 end
 
 function mymacro(v)
-    return "@innermacro@ + 10 + @innermacro@"
+    return "@innermacro@ + (function () return @innermacro@ end)() + @innermacro@"
 end
 
 print("comeco")
 
-f, e = load("abc=5; print(1 + @mymacro@ )")
-
-if e then
-    print(e)
-end
-
-if type(f) == 'function' then
-    f()
-elseif type(f) == 'table' then
-    for k, v in pairs(f) do
-        print(k, v)
-    end
-else
-   print('f não é function nem table, nil?')
-end
+load("abc=5; print(1 + @mymacro@)")()
 
 print("fim")
